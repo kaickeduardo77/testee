155b8ba4c7eb3cbb4537ef5dbee4195d64c98f56
KAICK EDUARDO ARAUJO DA SILVA <kaick.silva4@senaisp.edu.br>
2026-06-15 13:01:52 -0300

    part2

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,22 @@
 import 'dotenv/config';
 import express from 'express';
 import routes from './routes/routes.js';
-import cors from 'cors';
-import path from 'path';
+import { initializeDatabase } from './configs/Database.js';
+
 
 const app = express();
-app.use(express.static(path.join(process.cwd(), 'public')));
-app.use(cors());
-app.use(express.json()); 
+
+
+app.use(express.json());
 app.use('/', routes);
 
-app.listen(process.env.SERVER_PORT, () => {
-    console.log(`Servidor rodando em: http://localhost:${process.env.SERVER_PORT}`);
+
+initializeDatabase().then(() => {
+    app.listen(process.env.SERVER_PORT, () => {
+        console.log(`Servidor rodando na porta ${process.env.SERVER_PORT}`);
+    });
+}).catch(err => {
+    console.error("Erro ao inicializar o banco de dados:", err);
 });
+
+
